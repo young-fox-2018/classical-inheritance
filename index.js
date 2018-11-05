f7065f38cb17ae353c19f00767c5ceb679d9a211
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-11-05 19:39:14 +0700

    commit 1

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 "use strict"
+class Animal {
+    constructor(legs) {
+        this.legs = 2
+    }
+}
+
+class Bat extends Animal {
+    constructor(legs) {
+        super(legs)
+        this.foodType = 'herbivore'
+        this.flying = true
+        this.superPower = new SuperPower()
+    }
+}
+
+
+class SuperPower {
+    use_laser_vision() {
+        console.log('laser vision')
+    }
+}
+
+
+
+let bat = new Bat(2)
+bat.superPower.use_laser_vision()
+console.log(bat)
