ec29305f8bcb8882f370936d74dd2651a922546b
sendyakbar <msendyakbars@gmail.com>
2018-11-05 17:14:36 +0700

    classical inheritance done with module exports

---
+++ superPower.js
@@ -0,0 +1,13 @@
+'use strict'
+class SuperPower {
+    sonarNavigation() {
+        return 'sonar navigation animal'
+    }
+    nightVision() {
+        return 'night vision animal'
+    }
+    groupHunting() {
+        return 'group hunting animal'
+    }
+}
+module.exports = SuperPower
